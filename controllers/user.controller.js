96792e6e1de4c6548d3413c7a142d2a82a8395af
pick-cee <akinloluwaolumuyide@gmail.com>
2023-03-25 18:20:54 +0100

    added the comments functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -133,6 +133,103 @@ const getPost = async (request, response) => {
     }
 }
 
+// endpoint to comment on a post
+const commentPost = async(request, response)=>{
+    try{
+        const {comment} = request.body
+        const userId = request.query.userId
+        const postId = request.query.postId
+
+        const comment1 = await Comment.findOne({postId})
+        const post = await Blog.findById({_id: postId})
+        const user = await User.findById({_id: userId})
+
+        if (!post) {
+            return response.status(400).json({ message: "Post not found!" });
+        }
+        if (!user) {
+            return response.status(400).json({ message: "User not found!" });
+        }
+
+        const newComment = new Comment({
+            userId: userId,
+            postId: postId,
+            comment: comment
+        })
+        await newComment.save()
+        
+        post.comments.push({
+            commentId: newComment._id,
+            comment: comment,
+            user: user._id
+        })
+        await post.save()
+        return response.status(200).json({message: "Comment posted successfully", comment: newComment.comment})
+    }
+    catch(err){
+        console.log(err.message);
+        return response.status(500).json({message: "Server error, try again later!"})
+    }
+}
+
+// endpoint to get all comments under a post
+const getComments = async(request, response)=> {
+    try{
+        const blogId = request.query.blogId
+        const blog = await Blog.findById({_id: blogId})
+        if(!blog){
+            return response.status(400).json({message: "Blog post not found"})
+        }
+
+        const comments = await Comment.find({postId: blog._id}).sort({createdAt: -1})
+        return response.status(200).json({message: "Comments fetched successfully", comments: comments})
+    }
+    catch(err){
+        return response.status(500).json({message: "Server error, try again later!"})
+    }
+}
+
+// endpoint to delete a comment
+const deleteComments = async(request, response)=> {
+    try{
+        const userId = request.query.userId
+        const blogId = request.query.blogId
+        const commentId = request.query.commentId
+
+        const user = await User.findById({_id: userId})
+        const blog = await Blog.findById({_id: blogId})
+        const comment = await Comment.findById({_id: commentId})
+
+        if (!user) {
+            return response.status(400).json({ message: "User not found!" });
+        }
+        if (!blog) {
+            return response.status(400).json({ message: "Blog post not found!" });
+        }
+        if (!comment) {
+            return response.status(400).json({ message: "Comment not found!" });
+        }
+        let notInComment = true
+
+        blog.comments.forEach((comment, index) => {
+            if (comment.commentId == commentId) {
+                notInComment = false;
+                blog.comments.splice(index, 1);
+                blog.save();
+                return response.status(200).json({ message: "Comment deleted!" });
+            }
+        });
+        if (notInComment) {
+            return response.status(400).json({ message: "Comment not found!" });
+        }
+        await Comment.findByIdAndDelete(commentId);
+    }
+    catch(err){
+        return response.status(500).json({message: "Server error, try again later!"})
+    }
+}
+
 module.exports = {
-    register, login, getUser, makeBlog, deleteBlog, getPost
+    register, login, getUser, makeBlog, deleteBlog, getPost,
+    commentPost, getComments, deleteComments
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router()
 
 const {register, login, getUser,
-        makeBlog, deleteBlog, getPost} = require('../controllers/user.controller')
+        makeBlog, deleteBlog, getPost,
+        commentPost, deleteComments, getComments} = require('../controllers/user.controller')
 
 router.post('/register', register)
 router.post('/login', login)
@@ -9,5 +10,8 @@ router.get('/getUser', getUser)
 router.post('/makeBlog', makeBlog)
 router.delete('/deleteBlog', deleteBlog)
 router.get('/getBlog', getPost)
+router.post('/commentPost', commentPost)
+router.delete('/deleteComment', deleteComments)
+router.get('/getComments', getComments)
 
 module.exports = router
