62dbd899921fd0b662e1b80f7110f8ad17e1b23d
emirace <emmanuelakwuba57@gmail.com>
2023-09-10 17:58:04 +0100

    made Otp numeric

--- helpers/registerToken.js
+++ helpers/registerToken.js
@@ -1,15 +1,17 @@
-const RegisterOTP = require("../models/registerOTP")
-const User = require("../models/user.model")
-const randToken = require("rand-token")
+const RegisterOTP = require('../models/registerOTP');
+const User = require('../models/user.model');
+const randToken = require('rand-token').generator({
+  chars: '0-9',
+});
 
 const generateRegisterOTP = async (userId) => {
-    let random = randToken.generate(6)
-    const token = new RegisterOTP({
-        userId: userId,
-        OTP: random
-    })
-    await token.save()
-    return random
-}
+  let random = randToken.generate(6);
+  const token = new RegisterOTP({
+    userId: userId,
+    OTP: random,
+  });
+  await token.save();
+  return random;
+};
 
-module.exports = {generateRegisterOTP}
+module.exports = { generateRegisterOTP };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,45 +1,69 @@
-const router = require("express").Router()
+const router = require('express').Router();
 
-const {register, login, getUser,
-        makeBlog, deleteBlog, getPost,
-        commentPost, deleteComments, getComments,
-        likePost, dislikePost,
-        verifyEmailAddress, requestEmailToken, googleSignin, forgetPassword,
-        resetPassword, getAllBlogs, resumeBuilder, updateNumOfReads,
-        konectinInternshipMail, subscribeNewsLetter, unsubscribeNewsLetter,getUserResumes,
-        getUserResume,
-        updateUserResume,
-        createPdf
-} = require('../controllers/user.controller')
+const {
+  register,
+  login,
+  getUser,
+  makeBlog,
+  deleteBlog,
+  getPost,
+  commentPost,
+  deleteComments,
+  getComments,
+  likePost,
+  dislikePost,
+  verifyEmailAddress,
+  requestEmailToken,
+  googleSignin,
+  forgetPassword,
+  resetPassword,
+  getAllBlogs,
+  resumeBuilder,
+  updateNumOfReads,
+  konectinInternshipMail,
+  subscribeNewsLetter,
+  unsubscribeNewsLetter,
+  getUserResumes,
+  getUserResume,
+  updateUserResume,
+  createPdf,
+  verifyOtp,
+} = require('../controllers/user.controller');
 
-const {verifyUserToken} = require("../helpers/jsonwebtoken")
-const {isEmailVerified} = require("../helpers/isEmailVerified")
+const { verifyUserToken } = require('../helpers/jsonwebtoken');
+const { isEmailVerified } = require('../helpers/isEmailVerified');
 
-router.post('/register', register)
-router.post('/verifyEmail', verifyEmailAddress)
-router.post('/requestEmail', requestEmailToken)
-router.post('/googleSignIn', googleSignin)
-router.post('/login', login)
-router.post("/forgotPassword", forgetPassword)
-router.post("/resetPassword", resetPassword)
-router.get('/getUser', getUser)
-router.post('/makeBlog', verifyUserToken, isEmailVerified, makeBlog)
-router.delete('/deleteBlog', verifyUserToken, isEmailVerified, deleteBlog)
-router.get('/getBlog', verifyUserToken, isEmailVerified, getPost)
-router.get('/getAllBlogs', getAllBlogs)
-router.post('/commentPost', /*verifyUserToken,*/ isEmailVerified, commentPost)
-router.delete('/deleteComment', verifyUserToken, isEmailVerified, deleteComments)
-router.get('/getComments', isEmailVerified, getComments)
-router.post('/likePost', verifyUserToken, isEmailVerified, likePost)
-router.delete('/dislikePost', verifyUserToken, isEmailVerified, dislikePost)
-router.post("/resume", resumeBuilder)
-router.get("/getResumes", getUserResumes)
-router.get("/getResume", getUserResume)
-router.post("/createPdf",createPdf)
-router.put("/updateResume",updateUserResume)
-router.put("/updateNumOfReads", updateNumOfReads)
-router.post("/internshipMail", konectinInternshipMail)
-router.post("/subscribeMail", subscribeNewsLetter)
-router.post("/unsubscribeMail", unsubscribeNewsLetter)
+router.post('/register', register);
+router.post('/verifyEmail', verifyEmailAddress);
+router.post('/requestEmail', requestEmailToken);
+router.post('/googleSignIn', googleSignin);
+router.post('/login', login);
+router.post('/verify-otp', verifyOtp);
+router.post('/forgotPassword', forgetPassword);
+router.post('/resetPassword', resetPassword);
+router.get('/getUser', getUser);
+router.post('/makeBlog', verifyUserToken, isEmailVerified, makeBlog);
+router.delete('/deleteBlog', verifyUserToken, isEmailVerified, deleteBlog);
+router.get('/getBlog', verifyUserToken, isEmailVerified, getPost);
+router.get('/getAllBlogs', getAllBlogs);
+router.post('/commentPost', /*verifyUserToken,*/ isEmailVerified, commentPost);
+router.delete(
+  '/deleteComment',
+  verifyUserToken,
+  isEmailVerified,
+  deleteComments
+);
+router.get('/getComments', isEmailVerified, getComments);
+router.post('/likePost', verifyUserToken, isEmailVerified, likePost);
+router.delete('/dislikePost', verifyUserToken, isEmailVerified, dislikePost);
+router.post('/resume', resumeBuilder);
+router.get('/getResumes', getUserResumes);
+router.get('/getResume', getUserResume);
+router.post('/createPdf', createPdf);
+router.put('/updateResume', updateUserResume);
+router.put('/updateNumOfReads', updateNumOfReads);
+router.post('/internshipMail', konectinInternshipMail);
+router.post('/subscribeMail', subscribeNewsLetter);
+router.post('/unsubscribeMail', unsubscribeNewsLetter);
 
-module.exports = router
+module.exports = router;
