1fbb8ef57a43cfbb9cc8aa28fa82f280cb9e4303
Delight Fela-Steve <dfelastevegit@gmail.com>
2023-10-12 22:12:01 +0100

    removed resume validation

--- models/resume.model.js
+++ models/resume.model.js
@@ -7,98 +7,78 @@ const resumeSchema = new mongoose.Schema({
     basicInfo: {
         firstName: {
             type: String,
-            required: false
         },
         lastName: {
             type: String,
-            required: false
         },
         email: {
             type: String,
-            required: false
         },
         profileSummary: {
             type: String,
-            required: false
         },
         phoneNumber: {
             type: String,
-            required: false
         },
         country: {
             type: String,
-            required: false
         },
         city: {
             type: String,
-            required: false
         },
         state: {
             type: String,
-            required: false
         },
         zipCode: {
             type: String,
-            required: false
         },
         profession: {
             type: String,
-            required: false,
         }
     },
     jobExperience: [{
         jobTitle: {
             type: String,
-            required: false
         },
         company: {
             type: String,
-            required: false
         },
         country: {
             type: String,
-            required: false
         },
         city: {
             type: String,
-            required: false
         },
         state: {
             type: String,
-            required: false
         },
         startMonth: {
             type: String,
-            required: false
         },
         startYear: {
             type: String,
-            required: false
         },
         endMonth: {
             type: String,
-            required: false
         },
         endYear: {
             type: String,
-            required: false
         },
         workDesc: {
             type: String,
-            required: false
         },
     }],
     education: [{
-        schoolName: { type: String, required: false },
-        degree: { type: String, required: false },
-        country: { type: String, required: false },
-        city: { type: String, required: false },
-        state: { type: String, required: false },
-        graduated: { type: Boolean, required: false },
-        graduationMonth: { type: String, required: false },
-        graduationYear: { type: String, required: false },
+        schoolName: { type: String },
+        degree: { type: String },
+        country: { type: String },
+        city: { type: String },
+        state: { type: String },
+        graduated: { type: Boolean },
+        graduationMonth: { type: String },
+        graduationYear: { type: String },
     }],
-    skills: [{ type: String, required: false }],
+    skills: [{ type: String }],
     isCompleted: {
         type: Boolean,
         default: false
