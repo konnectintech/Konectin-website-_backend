dc98ee6b45df96394e55abd4170a24c1d846eb44
toby <leutoby@gmail.com>
2024-01-12 10:05:58 +0100

    chore: descriptive env vars

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -3,22 +3,22 @@ const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const cloudinaryUpload = (file) => {
-  return new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload(file)
-      .then((result) => {
-        resolve(result.secure_url);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader
+            .upload(file)
+            .then((result) => {
+                resolve(result.secure_url);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
 };
 
 module.exports = { cloudinaryUpload };
