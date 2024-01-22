a0630228f1e02af30abad385a52964de7febe715
ibotealaf <uknowbrave@gmail.com>
2024-01-22 11:05:52 +0100

    chore: email credentials from env var

--- config/email.js
+++ config/email.js
@@ -3,35 +3,35 @@ const { template } = require("../utils/mail-template");
 require("dotenv").config();
 
 const nm = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
-  port: 2525,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: true,
-  },
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: true,
+    },
 });
 const transporter = (email, subject, content, signature) => {
-  const htmltemplate = template.default(subject, content, signature);
-  return new Promise((resolve, reject) => {
-    nm.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: subject,
-      text: content,
-      html: htmltemplate,
-    })
-      .then((msg) => {
-        resolve(msg);
-      })
-      .catch((err) => {
-        console.error(err);
-        reject(err);
-      });
-  });
+    const htmltemplate = template.default(subject, content, signature);
+    return new Promise((resolve, reject) => {
+        nm.sendMail({
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: subject,
+            text: content,
+            html: htmltemplate,
+        })
+            .then((msg) => {
+                resolve(msg);
+            })
+            .catch((err) => {
+                console.error(err);
+                reject(err);
+            });
+    });
 };
 
 module.exports = { transporter };
