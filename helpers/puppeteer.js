0f5280378595d4687dae3d16c83129c7064a2ba9
Konectin <108042261+konnectintech@users.noreply.github.com>
2024-01-25 02:46:33 +0100

    Revert "Resume download v1"

--- helpers/puppeteer.js
+++ helpers/puppeteer.js
@@ -1,14 +1,11 @@
 const puppeteer = require("puppeteer");
 
-exports.createPdf = async () => {
+exports.createPdf = async (url, outputPath) => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   // Navigate to the specified URL
-  await page.goto(process.env.GET_CV_BY_ID_FRONT_END_URL, {
-    //GET_CV_BY_ID_FRONT_END_URL should equal to the endpoint to display the resume by id
-    waitUntil: "domcontentloaded",
-  });
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
   // Generate a PDF from the page content
   const pdfBuffer = await page.pdf({ format: "A4" });
