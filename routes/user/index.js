baaf9e63aa78aec5629755bebd34b8ce53fb7a65
naanmohammed <naanahmed24@gmail.com>
2024-02-17 17:59:21 +0300

    Add contact us route

--- routes/user/index.js
+++ routes/user/index.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 
 const authRoutes = require("./auth.routes");
 const resumeRoutes = require("./resume.routes");
+const contactRoutes = require("./contact-us.routes");
 
-router.use("",authRoutes, resumeRoutes);
+router.use("",authRoutes, resumeRoutes, contactRoutes);
 
 module.exports = router;
