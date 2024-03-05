eaf30cb149265b9220ad3933ba2d12d2feca54db
Jeanne d'Arc NYIRANWIZA <njoanc@gmail.com>
2024-03-05 16:04:54 +0200

    change():mailtrap host and post values

--- config/email.js
+++ config/email.js
@@ -3,8 +3,8 @@ const { template } = require("../utils/mail-template");
 require("dotenv").config();
 
 const nm = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
