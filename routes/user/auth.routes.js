f0a484b31639da96d3e2bdf3b41ce3f9647b8a98
naanmohammed <naanahmed24@gmail.com>
2024-03-11 16:20:28 +0300

    Add update notification routes controller

--- routes/user/auth.routes.js
+++ routes/user/auth.routes.js
@@ -13,5 +13,6 @@ router.post("/googleSignIn", auth.googleLogin);
 router.post("/verifyEmail", auth.verifyEmailAddress);
 router.delete("/logout", auth.logOut);
 router.delete("/removeEmail", auth.removeEmail);
+router.patch('/:userId/notifications', auth.updateNotificationPreferences);
 
 module.exports = router;
