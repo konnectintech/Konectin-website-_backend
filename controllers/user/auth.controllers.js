4de5d5e61d05e51613e6aa415497281d2ca00a74
naanmohammed <naanahmed24@gmail.com>
2024-03-11 16:20:02 +0300

    Update auth controller to add update notification preferences method

--- controllers/user/auth.controllers.js
+++ controllers/user/auth.controllers.js
@@ -414,3 +414,29 @@ exports.logOut = async function (req, res) {
     return res.status(500).json({ message: "Server error, try again later!" });
   }
 };
+
+exports.updateNotificationPreferences = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { resumeStatusUpdates, jobAlerts, internshipAlerts, blogUpdates, reminders } = req.body;
+    
+    const user = await User.findById(userId);
+    
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.notifications.resumeStatusUpdates = resumeStatusUpdates;
+    user.notifications.jobAlerts = jobAlerts;
+    user.notifications.internshipAlerts = internshipAlerts;
+    user.notifications.blogUpdates = blogUpdates;
+    user.notifications.reminders = reminders;
+
+    await user.save();
+
+    return res.status(200).json({ message: "Notification preferences updated successfully", user });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Server error, try again later" });
+  }
+};
