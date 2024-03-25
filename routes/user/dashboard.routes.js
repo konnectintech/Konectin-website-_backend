ac412cbff4e2652cc8749f1c7cfa273ef4202c3a
naanmohammed <naanahmed24@gmail.com>
2024-03-25 14:32:55 +0300

    Add route to update user picture

--- routes/user/dashboard.routes.js
+++ routes/user/dashboard.routes.js
@@ -5,5 +5,6 @@ router.get("/v2/getUserInfo", dashboard.getUserInfo);
 router.get("/v2/getNotificationSettings", dashboard.getNotificationSettings);
 router.get("/v2/getSocials", dashboard.getSocials);
 router.put("/v2/updateUser", dashboard.updateUser);
+router.put("/v2/updateUserPicture", dashboard.updateUserPicture);
 
 module.exports = router;
