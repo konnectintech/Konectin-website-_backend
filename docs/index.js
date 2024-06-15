e4ec578e87b0d64f039e0a8ef2247fff11c24024
Delight-Fela-Steve <dfelastevegit@gmail.com>
2024-06-15 15:57:20 +0100

    chore: modified swagger docs folder structure to make it easier to document

---
+++ docs/index.js
@@ -0,0 +1,10 @@
+const metadata = require("./metadata.json")
+const paths = require("./paths")
+const components = require("./components")
+
+
+module.exports = {
+    ...metadata,
+    paths,
+    components
+}
