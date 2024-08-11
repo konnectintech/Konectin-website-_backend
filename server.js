98675d70f44fa23af6f350b2aca0d9803e767928
Delight-Fela-Steve <dfelastevegit@gmail.com>
2024-08-11 14:05:55 +0100

    fix: increase payload size limit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(cors());
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   expressFileUpload({
